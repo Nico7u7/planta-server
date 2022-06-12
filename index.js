10640e0490cce9cea42ff29c8a34a2d3c80ea629
Nicolás Cofré <nicolas.cofre@alumnos.ucn.cl>
2022-06-11 21:42:42 -0400

    feat: add web socket server

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 const express = require('express');
+
+const app = express();
+
+//settings
+app.set('port', process.env.PORT || 3000);
+
+//start the server
+const server = app.listen(app.get('port'), () => {
+    console.log('server on port' , app.get('port'));
+})
+
+const socketIO = require('socket.io');
+const io = socketIO(server);
+
+//web sockets
+io.on('connection', () => {
+    console.log('new Connection');
+    socketIO.emit("test event","hola como estas");
+});
+
+io.on("connect_error", (err) => {
+    console.log(`connect_error due to ${err.message}`);
+});
